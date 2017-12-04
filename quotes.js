cef0ff6a6cb15be952ff48711d8c26c985448cd8
san00 <c5d59d07@opayq.com>
2017-12-04 15:02:19 +0000

    Undo remove json.parse

--- quotes.js
+++ quotes.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-  //initial quote
-  $('#quotetext').text('“It always seems impossible before it is done.”');
 
-  $('#quoteauthor').text('– Nelson Mandela');
+  //initial quote
+  $('#quote-text').text('“It always seems impossible before it is done.”');
+
+  $('#quote-author').text('- Nelson Mandela -');
 
   //call to get quotes
 
-  $("#quotebutton").on("click", function() {
+  $("#quotes-button").on("click", function() {
     $.ajax({
       headers: {
         "X-Mashape-Key": 'IhVmVOwricmshyoTy65WI12BITRKp1ZZNVwjsnOLQLiUaYkTrI',
@@ -16,22 +17,30 @@ $(document).ready(function() {
       },
       url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
       success: function(response) {
+        let data = JSON.parse(response);
+        $('#quote-text').text('"' + data.quote + '"' + "  ");
+        $('#quote-author').text("-" + " " + data.author + " " + "-");
 
-        $('#quotetext').text('"' + response.quote + '"' + "  ");
-        $('#quoteauthor').text("-" + " " + response.author);
       }
     });
   });
 
   //function to tweet quotes.
-  $('#mytwitterbutton').on('click', tweet)
+  $('#tweet-button').on('click', tweet)
 });
 
 function tweet() {
-  var randomQuote = document.getElementById('quotetext').textContent;
+  var randomQuote = document.getElementById('quote-text').textContent;
   var tweetUrl = ' https://twitter.com/intent/tweet?text=' + encodeURIComponent(randomQuote) +
     '&url=' +
     'https://codepen.io/osania/pen/mAYZVx';
 
   window.open(tweetUrl);
 };
+
+// #FAEEDE this colour was the best for accessibility
+// var colours = ["#e8d9dd","#b9e2f2","#f6e781","#faeede"];
+//
+// $("#quotebutton").on("click", function(){
+//   $('.flex-container').css("background-color", colours[Math.floor(Math.random() * colours.length)]);
+// })
